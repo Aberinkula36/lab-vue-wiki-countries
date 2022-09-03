c0ca1e54bd3e79b4b11b1f54c3d2d6bc71d1d3ee
Aberinkula36 <jmsaezteruel@hotmail.com>
2022-09-03 10:57:02 +0200

    iteration 1.4 completed

--- src/components/CountryDetails.vue
+++ src/components/CountryDetails.vue
@@ -1,7 +1,22 @@
 <template>
     <div class="container" v-if="selectedCountry">
-        <h1>{{  $route.params.id  }}</h1>
-        <p>{{  selectedCountry.name.common  }}</p>
+        <!-- <h1>{{  $route.params.id  }}</h1> -->
+        <img :src="`https://flagpedia.net/data/flags/icon/72x54/${selectedCountry.alpha2Code.toLowerCase()}.png`" alt="">
+        <h1>{{  selectedCountry.name.common  }}</h1>
+        <table>
+            <tr>
+                <th>Capital: </th>
+                <td>{{  selectedCountry.capital[0]  }}</td>
+            </tr>
+            <tr>
+                <th>Area: </th>
+                <td>{{  selectedCountry.area  }} km<sup>2</sup></td>
+            </tr>
+            <tr>
+                <th>Borders: </th>
+                <p v-for="border in selectedCountry.borders" :key="border"> <a v-bind:href="selectedCountry.name">{{  border  }}</a></p>
+            </tr>
+        </table>
     </div>
 </template>
 
@@ -10,7 +25,7 @@ export default {
     data() {
         return {
             countries: [],
-            selectedCountry: null
+            selectedCountry: null,
         }
     },
     async created() {
@@ -27,4 +42,20 @@ export default {
 </script>
 
 <style>
-</style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+      border-bottom: 1px solid #dddddd;
+    }
+    
+    td, th {
+      border-bottom: 1px solid #dddddd;
+      text-align: left;
+      padding: 20px;
+    }
+
+    table > tr > p {
+      text-align: left;
+      margin-left: 20px;
+    }
+    </style>
